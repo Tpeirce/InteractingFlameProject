e8842baeb758c69d62f47cf8c4d82fff6d546eb1
Tpeirce <Tpeirce@users.noreply.github.com>
2016-05-26 09:25:19 -0400

    Adding image centerline finder for use in edge-tracking (can split image, then search for each individual flame). Code is based on vector centerline finder

---
+++ imageCenterFinder.m
@@ -0,0 +1,22 @@
+function [ center ] = imageCenterFinder( I )
+
+%% work
+
+% based on my code for vector centerline finder
+
+% TODO: split along lateral position into two matrices, then
+% run xcor on the vectorized matrix to find best shift
+%%%%%%%%%%%% centerline %%%%%%%%%%%%%%%%%%%%%%%
+
+pass1 = I(:); 
+pass2 = fliplr(pass1); 
+shift = xcorr( pass1, pass2);
+[~, tctr ] = max(shift);
+center = (tctr)/size(I,1)/2; % (offset) + center, compensates for shift
+figure;
+imshow(I)
+hold on;
+plot([center, center], get(gca,'ylim'))
+hold off;
+
+end
